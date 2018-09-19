eb1bd760182be4bab3d362f50eff338257c5b383
olivierlubet <7896169+olivierlubet@users.noreply.github.com>
2018-09-19 21:45:07 +0200

    Change data location

--- src/main/scala/net/lubet/spaif/Context.scala
+++ src/main/scala/net/lubet/spaif/Context.scala
@@ -12,16 +12,19 @@ object Context {
     System.setSecurityManager(null)
 
     //val warehouseLocation = "file:///Users/olivi/IdeaProjects/funimmocrawl/spark"
-    val warehouseLocation = "file://" + new File("spark").
+    val dataLocation= new File("../data").
       getAbsolutePath.replace("C:\\", "/") // pour Windows
+
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
+    println(dataLocation+"/metastore_db")
     SparkSession
       .builder
-      .master("local[4]")
+      .master("local")
       .appName("FIC")
-      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .config("spark.sql.warehouse.dir", s"file://$dataLocation/warehouse")
+      .config("javax.jdo.option.ConnectionURL",s"jdbc:derby:;databaseName=$dataLocation/metastore_db;create=true")
       .config("hive.exec.dynamic.partition.mode","nonstrict")
       .config("spark.ui.port","8081")
       .enableHiveSupport()
